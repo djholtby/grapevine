e5c49419a2ba0e3c74c54cc987156bf3107ab531
Dan Holtby <djholtby@gmail.com>
2019-10-16 10:38:49 -0400

    add more fields to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,13 @@
 #lang info
 
-(define scribblings '(("scribblings/grapevine.scrbl" ())))
+(define pkg-name "grapevine")
+(define collection "grapevine")
+(define pkg-desc "Connect to the Grapevine intermud network")
+(define version "1.0")
+(define pkg-authors '(djholtby))
 
 (define deps '("base" "gregor" "rfc6455" "uuid"))
 (define build-deps '("racket-doc"
                      "scribble-lib"))
+
+(define scribblings '(("scribblings/grapevine.scrbl" ())))
